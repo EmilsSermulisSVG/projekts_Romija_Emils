4aa266f735664fdfc7a0fd889609c17d11fb18f6
EmilsSermulisSVG <144447091+EmilsSermulisSVG@users.noreply.github.com>
2025-04-04 09:14:42 +0300

    Add files via upload

---
+++ projekts_02_04.py
@@ -0,0 +1,36 @@
+from abc import ABC
+class Produkts(ABC):
+    def __init__(self,nosaukums,skaits,dienu_skaits,minimalais_skaits):
+        self.nosaukums = nosaukums
+        self.skaits = skaits
+        self.dienu_skaits = dienu_skaits
+        self.minimalais_skaits = minimalais_skaits
+
+siers = Produkts("Siers",10,5,8)
+
+class SvaigsProdukts(Produkts):
+    def __init__(self,nosaukums,skaits,dienu_skaits,minimalais_skaits):
+        super().__init__(nosaukums,skaits,dienu_skaits,minimalais_skaits)
+    
+class Tirgotajs:       
+    def __init__(self,tirgotajs):
+        self.tirgotajs = tirgotajs
+
+        if self.adresats = adresats:
+            pass
+        else:
+            print("Nepareizais adresāts!")
+
+
+
+maiznieks = Tirgotajs("Maiznieks")
+
+
+
+
+
+
+
+
+
+    
